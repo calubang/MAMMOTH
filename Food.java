76b78bf3730aa61346bb6d662aeb45230e376359
calubang <acalubang@gmail.com>
2019-03-21 08:55:53 +0900

    2019.03.21 abo delete
    delete~

--- Food.java
+++
@@ -1,8 +0,0 @@
-public interface Food {
-	public int getPrice();
-	public boolean setPrice(int price);
-	public String getName();
-	public boolean setName(String name);
-	public String toString();
-	public int getTotalPrice();
-}
